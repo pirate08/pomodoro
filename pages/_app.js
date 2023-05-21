c3794e73e45129678ecdd153f3a798d8933df941
Mayukh <mayukhdebgoswami82@gmail.com>
2023-05-22 01:11:27 +0530

    Chakra Ui is installed

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <div className={`${inter.className}`}>
+      <Head>
+        <title>Pomodoro - Timer</title>
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+       </ChakraProvider>
+    </div>
+  )
 }
