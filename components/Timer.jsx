77fab899cb35ec725b255ee0afa84b7d266c9113
Mayukh <mayukhdebgoswami82@gmail.com>
2023-05-23 01:57:58 +0530

    SetTimeout is added to the timer

--- components/Timer.jsx
+++ components/Timer.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Timer = () => {
-  return <div>Hello</div>;
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    let clock = setInterval(() => {
+      clearInterval(clock);
+      setSeconds(seconds - 1);
+
+      if (seconds === 0) {
+        if (minutes !== 0) {
+          setSeconds(59);
+          setMinutes(minutes - 1);
+        } else {
+          let minutes = 4;
+          let seconds = 59;
+          setMinutes(minutes);
+          setSeconds(seconds);
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(clock);
+  });
+
+  return (
+    <div className='h-screen w-full bg-cyan-900 flex flex-col items-center justify-center px-3'>
+      <div className='mb-8'>
+        <h1 className='text-3xl text-white font-bold'>Pomodoro - Timer</h1>
+      </div>
+      <div className='border-2 w-full md:w-2/4 py-16 md:py-28 shadow-2xl flex flex-col justify-center items-center'>
+        <div className='border-2 py-14 w-2/4 shadow-2xl text-white flex justify-center text-4xl md:text-6xl font-bold select-none'>
+          {minutes < 10 ? '0' + minutes : minutes}:
+          {seconds < 10 ? '0' + seconds : seconds}
+        </div>
+        <div className='mt-4'>Hellos</div>
+      </div>
+    </div>
+  );
 };
 
 export default Timer;
--- partials/Navbar.jsx
+++ partials/Navbar.jsx
@@ -1,27 +1,26 @@
-import { ReactNode } from 'react';
+// import { ReactNode } from 'react';
 import {
   Box,
+  Text,
   Flex,
-  Avatar,
   HStack,
   IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
   useDisclosure,
   useColorModeValue,
   Stack,
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const NavLink = [
   {
-    title: 'Dashboard',
-    link: '/',
+    title: 'Contact Us',
+    link: 'https://www.dashtower.com/contact',
+  },
+  {
+    title: 'View Website',
+    link: 'https://www.dashtower.com/',
   },
 ];
 
@@ -30,20 +29,35 @@ export default function Navbar() {
 
   return (
     <>
-      <Box bg={useColorModeValue('black', 'white')} color='white' px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+      <Box
+        bg={useColorModeValue('cyan.900', 'cyan.500')}
+        color='white'
+        px={4}
+        boxShadow='md'>
+        <Flex
+          h={20}
+          alignItems={'center'}
+          justifyContent={{ base: 'space-between', md: 'space-around' }}>
           <IconButton
             size={'md'}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
             aria-label={'Open Menu'}
             display={{ md: 'none' }}
+            color='black'
             onClick={isOpen ? onClose : onOpen}
           />
           <HStack spacing={8} alignItems={'center'}>
-            <Box>Logo</Box>
+            <Flex alignItems={'center'} cursor={'pointer'}>
+              <Image src='/logo.svg' width={40} height={30} />
+              <Text as='b' fontSize='md'>
+                Dashtower
+              </Text>
+            </Flex>
+          </HStack>
+          <Flex alignItems={'center'}>
             <HStack
               as={'nav'}
-              spacing={4}
+              spacing={6}
               display={{ base: 'none', md: 'flex' }}>
               {NavLink.map((link, index) => (
                 <Link href={link.link} key={index}>
@@ -51,37 +65,16 @@ export default function Navbar() {
                 </Link>
               ))}
             </HStack>
-          </HStack>
-          <Flex alignItems={'center'}>
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
-              </MenuList>
-            </Menu>
           </Flex>
         </Flex>
 
         {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
+          <Box pb={4} pt={2} display={{ md: 'none' }}>
             <Stack as={'nav'} spacing={4}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+              {NavLink.map((link, index) => (
+                <Link href={link.link} key={index}>
+                  {link.title}
+                </Link>
               ))}
             </Stack>
           </Box>
